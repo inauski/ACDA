904717f04513de9195f94b81e63d153ba0376526
inauski <santosinaki@users.noreply.github.com>
2017-11-02 16:44:29 +0100

    -

---
+++ Persistencia en ficheros/UT3-Herramientas de mapeo(Linq)/Tarea2-Consultar XML/Tarea2-Consultar XML/Tarea2-Consultar XML/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMostrarNomTel = New System.Windows.Forms.Button()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnMostrarNomTel
+        '
+        Me.btnMostrarNomTel.Location = New System.Drawing.Point(177, 37)
+        Me.btnMostrarNomTel.Name = "btnMostrarNomTel"
+        Me.btnMostrarNomTel.Size = New System.Drawing.Size(167, 23)
+        Me.btnMostrarNomTel.TabIndex = 0
+        Me.btnMostrarNomTel.Text = "Mostrar nombre y telefono"
+        Me.btnMostrarNomTel.UseVisualStyleBackColor = True
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(47, 37)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(93, 23)
+        Me.btnMostrar.TabIndex = 1
+        Me.btnMostrar.Text = "Mostrar nombres"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(47, 76)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(101, 186)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(177, 76)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(226, 186)
+        Me.ListBox2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(415, 317)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.btnMostrarNomTel)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnMostrarNomTel As System.Windows.Forms.Button
+    Friend WithEvents btnMostrar As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Persistencia en ficheros/UT3-Herramientas de mapeo(Linq)/Tarea3-ExpresionesIncrustadas(I)/Tarea3-ExpresionesIncrustadas(I)/Tarea3-ExpresionesIncrustadas(I)/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Private Sub btnBailes1_Click(sender As System.Object, e As System.EventArgs) Handles btnBailes1.Click
+        'Dim baile As XDocument =
+        '<?xml version="1.0" encoding="UTF-8"?>
+        '<Bailes>
+        '    <baile id="1">
+        '        <nombre>Tango</nombre>
+        '        <precio cuota="mensual">37</precio>
+        '        <plazas>16</plazas>
+        '        <comienzo>2012-09-01</comienzo>
+        '        <fin>2012-12-01</fin>
+        '        <profesor>Juan Diego</profesor>
+        '        <sala>A1</sala>
+        '    </baile>
+        '    <baile id="2">
+        '        <nombre>Cha-cha-cha</nombre>
+        '        <precio cuota="trimestral">90</precio>
+        '        <plazas>12</plazas>
+        '        <comienzo>2012-02-01</comienzo>
+        '        <fin>2012-11-01</fin>
+        '        <profesor>Maitetxu</profesor>
+        '        <sala>A1</sala>
+        '    </baile>
+        '    <baile id="3">
+        '        <nombre>Rock</nombre>
+        '        <precio cuota="mensual">43</precio>
+        '        <plazas>15</plazas>
+        '        <comienzo>2012-01-01</comienzo>
+        '        <fin>2012-06-01</fin>
+        '        <profesor>Saioa</profesor>
+        '        <sala>A1</sala>
+        '    </baile>
+        '    <baile id="4">
+        '        <nombre>Bachata</nombre>
+        '        <precio cuota="mensual">25</precio>
+        '        <plazas>18</plazas>
+        '        <comienzo>2012-09-01</comienzo>
+        '        <fin>2012-12-01</fin>
+        '        <profesor>Paco</profesor>
+        '        <sala>A3</sala>
+        '    </baile>
+        '    <baile id="5">
+        '        <nombre>Salsa</nombre>
+        '        <precio cuota="trimestral">75</precio>
+        '        <plazas>22</plazas>
+        '        <comienzo>2012-02-01</comienzo>
+        '        <fin>2012-08-01</fin>
+        '        <profesor>Marieta</profesor>
+        '        <sala>A3</sala>
+        '    </baile>
+        '    <baile id="6">
+        '        <nombre>Pasodoble</nombre>
+        '        <precio cuota="anual">180</precio>
+        '        <plazas>12</plazas>
+        '        <comienzo>2012-01-01</comienzo>
+        '        <fin>2013-01-01</fin>
+        '        <profesor>Fatima</profesor>
+        '        <sala>A3</sala>
+        '    </baile>
+        '</Bailes>
+
+        'En vez de escribir todo el xml, se puede crear más fácil con estas dos lineas
+        Dim baile = XDocument.Load("Bailes.xml")
+        TextBox1.Text = baile.ToString
+
+        baile.Save("Bailes.xml")
+        Dim bailes = XDocument.Load("Bailes.xml")
+        TextBox1.Text = bailes.ToString
+    End Sub
+
+    Private Sub btnBailes2_Click(sender As System.Object, e As System.EventArgs) Handles btnBailes2.Click
+        Dim bailes2 = XDocument.Load("Bailes.xml")
+
+        Dim consulta As XDocument =
+            <?xml version="1.0" encoding="utf-8"?>
+            <Bailes>
+                <%= From bai In bailes2...<baile>
+                    Where bai.<plazas>.Value > 15
+                    Select bai
+                %>
+            </Bailes>
+
+        consulta.Save("Bailes2.xml")
+        Dim b = XDocument.Load("Bailes2.xml")
+        TextBox2.Text = b.ToString
+
+        'Otra forma de hacerlo, se hace la consutla fuera y luego se pregunta por ella 
+        'Dim doc As XDocument = XDocument.Load("bailes.xml")
+        'Dim consulta = _
+        '        From baile In doc...<baile>
+        '        Where (baile.<plazas>.Value > 15)
+        '        Select baile
+        'Dim docAux As XDocument = _
+        '        <?xml version="1.0" encoding="UTF-8"?>
+        '        <bailes>
+        '            <%= consulta %>
+        '        </bailes>
+
+        'docAux.Save("bailes2.xml")
+        'MessageBox.Show("Archivo creado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        'Private Function docAux() As Object
+        '    Throw New NotImplementedException
+        'End Function
+    End Sub
+End Class
