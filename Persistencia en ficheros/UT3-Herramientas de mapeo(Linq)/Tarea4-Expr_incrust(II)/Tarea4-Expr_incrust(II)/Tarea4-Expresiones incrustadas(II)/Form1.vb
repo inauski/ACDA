900290f2f3d5d3792c819f78b987c2861299f9b0
inauski <santosinaki@users.noreply.github.com>
2017-11-03 16:17:53 +0100

    -

---
+++ Persistencia en ficheros/UT3-Herramientas de mapeo(Linq)/Tarea4-Expr_incrust(II)/Tarea4-Expr_incrust(II)/Tarea4-Expresiones incrustadas(II)/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub btnBailes1_Click(sender As System.Object, e As System.EventArgs) Handles btnBailes1.Click
+        Dim baile = XDocument.Load("Bailes.xml")
+        TextBox1.Text = baile.ToString
+
+        baile.Save("Bailes.xml")
+        Dim bailes = XDocument.Load("Bailes.xml")
+        TextBox1.Text = bailes.ToString
+    End Sub
+
+    Private Sub btnActividades_Click(sender As System.Object, e As System.EventArgs) Handles btnActividades.Click
+        Dim bailes2 = XDocument.Load("Bailes.xml")
+
+        'Modificar XML
+        Dim consulta As XElement =
+            <Actividades>
+                <%= From bai In bailes2...<baile>
+                    Select <actividad>
+                               <%= bai.<nombre> %>
+                               <%= bai.<precio> %>
+                               <%= bai.<profesor> %>
+                           </actividad>
+                %>
+            </Actividades>
+
+        consulta.Save("Actividades.xml")
+        Dim b = XDocument.Load("Actividades.xml")
+        TextBox2.Text = b.ToString
+    End Sub
+End Class
